917aa1bf7595e1219adefc3e979b4f6a0d827c89
Jason Hyde <github@2bad.me>
2023-04-13 17:30:16 +0200

    [conf] define tests level and ignore build folder

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     globals: true,
+    exclude: ['build'],
     coverage: {
-      provider: 'c8',
-    },
-  },
-});
+      provider: 'c8'
+    }
+  }
+})
