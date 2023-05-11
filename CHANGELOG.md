1410b901d1bd7e30a66e4cbf0acca8f0b3be59a6
Jason Hyde <github@2bad.me>
2023-05-11 20:05:29 +0200

    [ts] add missing cookie and set user-agent header

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,7 +17,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Removed
 
-## [2.0.1] - 2023-05-11
+## [2.0.2] - 2023-05-11
+
+### Fixed
+
+- Add missing `mkt` cookie
+- Set `user-agent` header to valid browser
+
+## [2.0.1] - 2023-05-10
 
 ### Fixed
 
--- source/client.ts
+++ source/client.ts
@@ -1,5 +1,5 @@
 import { got } from 'got'
-import { CookieJar, type Cookie } from 'tough-cookie'
+import { CookieJar, Cookie } from 'tough-cookie'
 
 /**
  * Extend `got` http client so every time request is made we check if there is a `rid.sig` cookie
@@ -19,12 +19,26 @@ export const get = got.extend({
         const ridSigCookie = cookies.find((cookie: { key: string }) => cookie.key === 'rid.sig')
         if (typeof ridSigCookie === 'undefined') {
           // If it doesn't exist, visit the Ryanair site to set the cookie
+
+          // @ts-expect-error got is unable to resolve correct type for cookieJar
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+          options.cookieJar?.setCookieSync(
+            new Cookie({
+              key: 'mkt',
+              value: '/de/de/',
+              domain: 'ryanair.com'
+            }),
+            'https://www.ryanair.com'
+          )
           await got('https://www.ryanair.com/de/de', {
             cookieJar: options.cookieJar
           })
         }
       }
     ]
+  },
+  headers: {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; rv:113.0) Gecko/20100101 Firefox/113.0'
   },
   resolveBodyOnly: true,
   responseType: 'json',
