777fdcb36325774a2eda6699155cf2c62236f192
Jason Hyde <github@2bad.me>
2023-05-12 05:17:04 +0200

    [ts] add utility function to generate array of dates for each month in specific range

--- source/utils/date.ts
+++ source/utils/date.ts
@@ -1,6 +1,8 @@
+import { eachMonthOfInterval, format } from 'date-fns'
+import { type StrDate } from '~/date.types'
+
 /**
- * Calculates the date for the next day in 'YYYY-MM-DD' format.
- * @returns {string} The formatted date for tomorrow.
+ * Calculates the date for the next day (formatted as 'YYYY-MM-DD')
  */
 
 export const tomorrow = (): string => {
@@ -9,4 +11,16 @@ export const tomorrow = (): string => {
 
   // Format the date into 'YYYY-MM-DD' format
   return currentDate.toISOString().slice(0, 10)
+}
+
+/**
+ * Returns an array of the first day of each month in a given date range (formatted as 'YYYY-MM-01')
+ *
+ * @param startDate - The start date of the range (formatted as 'YYYY-MM-DD')
+ * @param endDate - The end date of the range (formatted as 'YYYY-MM-DD')
+ *
+ */
+export const getFirstDayOfEachMonthInRange = (startDate: StrDate, endDate: StrDate): StrDate[] => {
+  const dates = eachMonthOfInterval({ start: new Date(startDate), end: new Date(endDate) })
+  return dates.map((d) => format(d, 'yyyy-MM-01'))
 }
