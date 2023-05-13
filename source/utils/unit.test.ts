8737b9c1b936e005dc8c423b47d50f4778030fae
Jason Hyde <github@2bad.me>
2023-05-13 04:29:42 +0200

    [test] add tests for new date utility functions

--- source/utils/unit.test.ts
+++ source/utils/unit.test.ts
@@ -1,31 +1,88 @@
 import { StrDate } from '~/date.types'
-import { tomorrow } from '~/utils/date'
+import { getFirstDayOfEachMonthInRange, nextMonth, tomorrow } from '~/utils/date'
 
-describe('tomorrow', () => {
-  it('should return a string in YYYY-MM-DD format', () => {
-    const result = tomorrow()
-    expect(result).toMatch(/^\d{4}-\d{2}-\d{2}$/)
+describe('date', () => {
+  describe('tomorrow', () => {
+    it('should return a string in YYYY-MM-DD format', () => {
+      const result = tomorrow()
+      expect(result).toMatch(/^\d{4}-\d{2}-\d{2}$/)
+    })
+
+    it('should return a valid date', () => {
+      const result = tomorrow()
+      const [year, month, day] = result.split('-').map((part) => parseInt(part))
+
+      // @ts-expect-error even if the values are going to be undefined the test will cover it
+      const date = new Date(year, month - 1, day)
+
+      expect(date.getFullYear()).toBe(year)
+      expect(date.getMonth() + 1).toBe(month)
+      expect(date.getDate()).toBe(day)
+    })
   })
 
-  it('should return a valid date', () => {
-    const result = tomorrow()
-    const [year, month, day] = result.split('-').map((part) => parseInt(part))
+  describe('nextMonth', () => {
+    it('should return a string in YYYY-MM-DD format', () => {
+      const result = nextMonth()
+      expect(result).toMatch(/^\d{4}-\d{2}-\d{2}$/)
+    })
 
-    // @ts-expect-error even if the values are going to be undefined the test will cover it
-    const date = new Date(year, month - 1, day)
+    it('should return a valid date', () => {
+      const result = nextMonth()
+      const [year, month, day] = result.split('-').map((part) => parseInt(part))
 
-    expect(date.getFullYear()).toBe(year)
-    expect(date.getMonth() + 1).toBe(month)
-    expect(date.getDate()).toBe(day)
+      // @ts-expect-error even if the values are going to be undefined the test will cover it
+      const date = new Date(year, month - 1, day)
+
+      expect(date.getFullYear()).toBe(year)
+      expect(date.getMonth() + 1).toBe(month)
+      expect(date.getDate()).toBe(day)
+    })
   })
 
-  it('should pass zod type validation', () => {
-    const dateString = '2022-01-31'
-    expect(StrDate.parse(dateString)).toEqual(dateString)
+  describe('getFirstDayOfEachMonthInRange', () => {
+    it('returns an array', () => {
+      const result = getFirstDayOfEachMonthInRange('2020-01-01', '2020-12-31')
+      expect(Array.isArray(result)).toBe(true)
+    })
+
+    it('throws a RangeError when start date is after end date', () => {
+      expect(() => getFirstDayOfEachMonthInRange('2021-01-01', '2020-12-31')).toThrow(RangeError)
+    })
+
+    it('returns an array of length 1 when start and end dates are in the same month', () => {
+      const result = getFirstDayOfEachMonthInRange('2022-05-15', '2022-05-20')
+      expect(result).toHaveLength(1)
+      expect(result[0]).toBe('2022-05-01')
+    })
+
+    it('returns an array containing first day of each month within the given range', () => {
+      const result = getFirstDayOfEachMonthInRange('2023-06-15', '2024-03-10')
+      expect(result).toHaveLength(10)
+      expect(result).toEqual([
+        '2023-06-01',
+        '2023-07-01',
+        '2023-08-01',
+        '2023-09-01',
+        '2023-10-01',
+        '2023-11-01',
+        '2023-12-01',
+        '2024-01-01',
+        '2024-02-01',
+        '2024-03-01'
+      ])
+    })
   })
 
-  it('should throw error for wrong date string format', () => {
-    const dateString = '2022/01/31'
-    expect(() => StrDate.parse(dateString)).toThrowError()
+  describe('zod StrDate type', () => {
+    it('should pass zod type validation', () => {
+      const dateString = '2022-01-31'
+      expect(StrDate.parse(dateString)).toEqual(dateString)
+    })
+
+    it('should throw error for wrong date string format', () => {
+      const dateString = '2022/01/31'
+      expect(() => StrDate.parse(dateString)).toThrowError()
+    })
   })
 })
