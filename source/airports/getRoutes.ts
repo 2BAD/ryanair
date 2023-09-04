f8a4234066c87b0401b503455ca7d8990e3fef01
Jason Hyde <github@2bad.me>
2023-09-04 21:17:29 +0200

    [ts] implement the function that will return the available route between two airports

---
+++ source/airports/getRoutes.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod'
+import * as airports from '~/airports/index.ts'
+import { IataCode, type Destination } from './types.js'
+
+/**
+ * Retrieves IATA codes from an array of destinations.
+ *
+ * @param destinations - An array of Destination objects.
+ */
+const getIataCodes = (destinations: Destination[]): Set<IataCode> => {
+  return new Set(destinations.map((airport) => airport.arrivalAirport.code))
+}
+
+/**
+ * Retrieves available routes between two airports.
+ *
+ * @param from - The departure airport IATA code.
+ * @param to - The arrival airport IATA code.
+ */
+export const getRoutes = async (from: IataCode, to: IataCode): Promise<IataCode[][]> => {
+  const [departureDestinations, arrivalDestinations] = await Promise.all([
+    airports.getDestinations(from),
+    airports.getDestinations(to)
+  ])
+
+  const departureConnectionsCodes = getIataCodes(departureDestinations)
+  const arrivalConnectionsCodes = getIataCodes(arrivalDestinations)
+
+  if (departureConnectionsCodes.has(to)) return [[from, to]]
+
+  let matchingCodes = [...departureConnectionsCodes].filter((code) => arrivalConnectionsCodes.has(code))
+  matchingCodes = z.array(IataCode).parse(matchingCodes)
+  const routes = matchingCodes.map((code) => [from, code, to])
+  return routes
+}
