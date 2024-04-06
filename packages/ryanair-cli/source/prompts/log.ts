44468d50638fbe84e40ad5d68baaf514e9a389b4
Jason Hyde <github@2bad.me>
2024-04-06 07:03:24 +0200

    [misc] fix missing jsdoc

--- packages/ryanair-cli/source/prompts/log.ts
+++ packages/ryanair-cli/source/prompts/log.ts
@@ -1,8 +1,9 @@
 import { symbols } from './symbols.ts'
 
 /**
+ * Prettify notification messages by appending symbols
  *
- * @param text
+ * @param text - Text that should be logged
  */
 export const notify = (text: string): void => {
   console.log(`${symbols.notice} ${text}`)
