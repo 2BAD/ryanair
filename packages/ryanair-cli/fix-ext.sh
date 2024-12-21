841b3ce245164b269a50d8edcc52e60d176ae0f0
Jason Hyde <github@2bad.me>
2024-12-21 07:39:39 +0100

    [conf] drop fix-ext.sh in favor of @2bad/tsfix

--- packages/ryanair-cli/fix-ext.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-directory="./build"
-files=$(find "$directory" -type f \( -name "*.js" -o -name "*.d.ts" \))
-
-for file in $files; do
-  sed -i 's/\.\([^.]*\)\.ts/\.\1.js/g' "$file"
-  echo "Replaced extensions in $file"
-done
